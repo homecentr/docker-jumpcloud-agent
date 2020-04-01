5370ecdcc62de010206e3be91b17a0bab5c915f4
Lukas Holota <l.holota@outlook.com>
2020-04-01 22:43:41 +0200

    Debug

--- tests/src/test/java/Secrets.java
+++ tests/src/test/java/Secrets.java
@@ -1,5 +1,7 @@
 public class Secrets {
     public static String getConnectKey() {
+        System.out.println("CONNECT KEY=" + System.getProperty("connect_key"));
+
         return System.getProperty("connect_key");
     }
 
